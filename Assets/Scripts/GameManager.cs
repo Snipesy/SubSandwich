a9d0d9cc7865d73f7b1ccb5247e644ce0f0dc4ea
Snipesy <justin.hoogestraat@jacks.sdstate.edu>
2016-07-06 17:13:53 -0500

    Revert "fix project..."
    This reverts commit 9a2bf6fedc01e58a683accef4b3511444f54e019.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+
+
+    public NetworkManager hello;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        bool z = Input.GetButtonDown("Fire1");
+
+        if (z)
+        {
+            return;
+        }
+
+
+    }
+}
